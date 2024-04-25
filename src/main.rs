4f8bc7b6588bd00515b32fba8e09998b37b65a34
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-25 14:22:09 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use rocket::{get, launch, routes};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![root])
+}
+
+#[get("/")]
+async fn root() -> String {
+    "Hello, World".to_string()
+}
